7eaf813804265139c2e4f3046c6bd6f67a354a11
Sergey <serega1234567089@gmail.com>
2020-07-15 18:29:50 +0700

    first stage

--- Sergey/src/main/kotlin/arithmetic.kt
+++ Sergey/src/main/kotlin/arithmetic.kt
@@ -1,16 +1,158 @@
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 interface Expression {
     fun getVal(): Double
 }
-class Const(val num: Double): Expression {
+
+open class Const(private val num: Double): Expression {
     override fun getVal(): Double{
         return num
     }
 }
-interface isSingleArgumentOperation{
+
+interface IsSingleArgumentOperation{
     fun apply(a: Expression): Double
 }
-enum class SingleArgumentOperation: isSingleArgumentOperation{
-    fun Inc(val a: Double): Expression{
-        override fun apply()
+
+interface IsTwoArgumentOperation{
+    fun apply(a: Expression, b: Expression): Double
+}
+
+interface  IsManyArgumentOperation{
+    fun apply(nums: Array<Expression>): Double
+}
+
+enum class SingleArgumentOperation: IsSingleArgumentOperation{
+    Inc {
+        override fun apply(a: Expression): Double = a.getVal() + 1.0
+    },
+    Dec {
+        override fun apply(a: Expression): Double = a.getVal() - 1.0
+    },
+    Sqr {
+        override fun apply(a: Expression): Double = a.getVal() * a.getVal()
+    },
+    Sqrt {
+        override fun apply(a: Expression): Double = sqrt(a.getVal())
     }
+}
+
+enum class TwoArgumentOperation: IsTwoArgumentOperation{
+    Sum{
+        override fun apply(a: Expression, b: Expression): Double = a.getVal() + b.getVal()
+    },
+    Sub{
+        override fun apply(a: Expression, b: Expression): Double = a.getVal() - b.getVal()
+    },
+    Mul{
+        override fun apply(a: Expression, b: Expression): Double = a.getVal() * b.getVal()
+    },
+    Div{
+        override fun apply(a: Expression, b: Expression): Double = a.getVal() / b.getVal()
+    },
+    Pow{
+        override fun apply(a: Expression, b:Expression): Double = a.getVal().pow(b.getVal())
+    }
+}
+
+enum class ManyArgumentOperation: IsManyArgumentOperation{
+    Max{
+        override fun apply(nums: Array <Expression>): Double{
+            var cnt = nums[0].getVal()
+            for (i in 1 until nums.size){
+                if (cnt < nums[i].getVal()) cnt = nums[i].getVal()
+            }
+            return cnt
+        }
+    },
+    Min{
+        override fun apply(nums: Array <Expression>): Double{
+            var cnt = nums[0].getVal()
+            for (i in 1 until nums.size){
+                if (cnt > nums[i].getVal()) cnt = nums[i].getVal()
+            }
+            return cnt
+        }
+    },
+    Avg{
+        override fun apply(nums: Array <Expression>): Double{
+            var cnt = 0.0
+            for (i in nums.indices){
+                cnt += nums[i].getVal()
+            }
+            cnt /= nums.size
+            return cnt
+        }
+    }
+}
+
+class SingleArgumentExpression(
+    private val ac: IsSingleArgumentOperation,
+    private val expression: Expression
+): Expression{
+    override fun getVal(): Double {
+        return ac.apply(expression)
+    }
+    companion object Operations{
+        fun inc(a: Expression): SingleArgumentExpression =
+            SingleArgumentExpression(SingleArgumentOperation.Inc, a)
+
+        fun dec(a: Expression): SingleArgumentExpression =
+            SingleArgumentExpression(SingleArgumentOperation.Dec, a)
+
+        fun sqr(a: Expression): SingleArgumentExpression =
+            SingleArgumentExpression(SingleArgumentOperation.Sqr, a)
+
+        fun sqrt(a: Expression): SingleArgumentExpression =
+            SingleArgumentExpression(SingleArgumentOperation.Sqrt, a)
+    }
+}
+
+class TwoArgumentExpression(
+    private val ac: IsTwoArgumentOperation,
+    private val e1: Expression,
+    val e2: Expression
+): Expression{
+    override fun getVal(): Double {
+        return ac.apply(e1, e2)
+    }
+    companion object Operations{
+        fun sum(e1: Expression, e2: Expression): TwoArgumentExpression =
+            TwoArgumentExpression(TwoArgumentOperation.Sum, e1, e2)
+
+        fun sub(e1: Expression, e2: Expression): TwoArgumentExpression =
+            TwoArgumentExpression(TwoArgumentOperation.Sub, e1, e2)
+
+        fun mul(e1: Expression, e2: Expression): TwoArgumentExpression =
+            TwoArgumentExpression(TwoArgumentOperation.Mul, e1, e2)
+
+        fun div(e1: Expression, e2: Expression): TwoArgumentExpression =
+            TwoArgumentExpression(TwoArgumentOperation.Div, e1, e2)
+
+        fun pow(e1: Expression, e2: Expression): TwoArgumentExpression =
+            TwoArgumentExpression(TwoArgumentOperation.Pow, e1, e2)
+    }
+}
+
+class ManyArgumentExpression(
+    private val ac: IsManyArgumentOperation,
+    private val nums: Array <Expression>
+): Expression {
+    override fun getVal(): Double {
+        return ac.apply(nums)
+    }
+    companion object Operations{
+        fun max(nums: Array<Expression>): ManyArgumentExpression =
+            ManyArgumentExpression(ManyArgumentOperation.Max, nums)
+
+        fun min(nums: Array<Expression>): ManyArgumentExpression =
+            ManyArgumentExpression(ManyArgumentOperation.Min, nums)
+
+        fun avg(nums: Array<Expression>): ManyArgumentExpression =
+            ManyArgumentExpression(ManyArgumentOperation.Avg, nums)
+    }
+}
+
+fun main(){
 }
---
+++ Sergey/src/test/kotlin/test.kt
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test
+import kotlin.math.sqrt
+import org.junit.jupiter.api.Assertions.*
+
+
+class Junit5Test{
+    @Test
+    fun testSingle(){
+        assertEquals(SingleArgumentExpression.inc(Const(1456.0)).getVal(), 1457.0, 0.00001)
+        assertEquals(SingleArgumentExpression.dec(Const(3.1415)).getVal(), 2.1415, 0.00001)
+        assertEquals(SingleArgumentExpression.sqr(Const(37.0)).getVal(), 1369.0, 0.00001)
+        assertEquals(SingleArgumentExpression.sqrt(Const(66.0)).getVal(), 8.124038, 0.00001)
+    }
+    @Test
+    fun testTwo(){
+        assertEquals(TwoArgumentExpression.sum(Const(3.1415),
+            Const(2.7182)).getVal(), 5.8597, 0.00001)
+
+        assertEquals(TwoArgumentExpression.sub(Const(3.1415),
+            Const(2.7182)).getVal(), 0.4233, 0.00001)
+
+        assertEquals(TwoArgumentExpression.mul(Const(3.1),
+            Const(200.7)).getVal(), 622.17, 0.00001)
+
+        assertEquals(TwoArgumentExpression.div(Const(3.0),
+            Const(2.5)).getVal(), 1.2, 0.00001)
+
+        assertEquals(TwoArgumentExpression.pow(Const(3.0),
+            Const(5.0)).getVal(), 243.0, 0.00001)
+    }
+    @Test
+    fun testMany(){
+        val arr = arrayOf<Expression>(Const(1.0), Const(4.9), Const(5.0), Const(3.0))
+        assertEquals(ManyArgumentExpression.max(arr).getVal(), 5.0, 0.00001)
+        assertEquals(ManyArgumentExpression.min(arr).getVal(), 1.0, 0.00001)
+        assertEquals(ManyArgumentExpression.avg(arr).getVal(), 3.475, 0.00001)
+    }
+}
