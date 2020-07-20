ca372358a960bf6ac79e27900715dc8368946f4c
evgenius2020 <zavarzinevg@gmail.com>
2020-07-20 17:19:52 +0700

    epic nextId

--- Call of Anoro€/src/jvmMain/kotlin/engine/Entity.kt
+++ Call of Anoro€/src/jvmMain/kotlin/engine/Entity.kt
@@ -1,8 +1,11 @@
 package engine
 
+private var nextId: Int = 1
+
 open class Entity(
     var x: Double = 0.0,
     var y: Double = 0.0,
-    var angle: Double = 0.0
+    var angle: Double = 0.0,
+    val id: Int = nextId++
 )
 
