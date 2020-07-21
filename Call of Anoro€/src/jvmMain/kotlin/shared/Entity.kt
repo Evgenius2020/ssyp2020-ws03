2404b4e2a0b8d4d187ad5ffd68cde0cfb61d71d3
Sergey <serega1234567089@gmail.com>
2020-07-21 17:11:21 +0700

    sth

---
+++ Call of Anoro€/src/jvmMain/kotlin/shared/Entity.kt
@@ -0,0 +1,11 @@
+package shared
+
+private var nextId: Int = 1
+
+open class Entity(
+    var x: Double = 0.0,
+    var y: Double = 0.0,
+    var angle: Double = 0.0,
+    val id: Int = nextId++
+) : java.io.Serializable
+
