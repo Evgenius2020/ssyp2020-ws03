b2f39ac5ba5b91a4905f937711fb036052635a60
Andrey <ermolin2003@gmail.com>
2020-07-22 16:36:25 +0700

    Add TeamsManager

---
+++ Call of Anoro€/src/jvmMain/kotlin/engine/managers/TeamsManager.kt
@@ -0,0 +1,34 @@
+package engine.managers
+
+import shared.Entity
+import shared.Player
+
+data class TeamInfo(
+        val nicknames: MutableList<String>,
+        var score: Double = 0.0
+)
+
+class TeamsManager : BaseManager<Unit>() {
+    private val teamcount = 2
+    private val teams = List<TeamInfo>(teamcount) { TeamInfo(mutableListOf<String>(), 0.0) }
+
+    fun register(player: Player) {
+        super.register(player, Unit)
+        player.team = nextTeam()
+        addPlayer(player.team, player.nick)
+    }
+    fun removePlayer(player: Player) {
+        removePlayer(player.team, player.nick)
+        super.delete(player)
+    }
+
+    private fun nextTeam() = teams.withIndex().minBy { (_, f) -> f.nicknames.size }?.index ?: 0
+    private fun addPlayer(team: Int, nickname: String) = teams[team]!!.nicknames.add(nickname)
+    private fun removePlayer(team: Int, nickname: String) = teams[team]!!.nicknames.remove(nickname)
+
+    fun addScore(team: Int, score: Double) {
+        teams[team]!!.score += score
+    }
+    fun getNames(team: Int) = teams[team].nicknames.toTypedArray()
+    fun getScore(team: Int) = teams[team].score
+}
