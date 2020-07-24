0413dc89806b8ae7d987a466dc0d441022e1de54
Anton Plusnin <anton@vipset.com>
2020-07-24 19:42:13 +0700

    GUI FIXEDDDDDDDDDDDDD

--- Call of Anoro€/client/src/jvmMain/kotlin/Statistics.kt
+++ Call of Anoro€/client/src/jvmMain/kotlin/Statistics.kt
@@ -11,17 +11,18 @@ class Statistics(_w: Double, _h: Double) : FixedSizeContainer(_w, _h) {
     private var team2score: View
     private var team1 = mutableMapOf<String, View>()
     private var team2 = mutableMapOf<String, View>()
-
+    private val W = _w
+    private val H = _h
     init {
-        blackboard = solidRect(width, height, RGBA(0, 0, 0, 150)).xy(0, 0)
+        blackboard = solidRect(W, H, RGBA(0, 0, 0, 150)).xy(0, 0)
 
         centerLine = solidRect(1, 1, RGBA(0, 0, 0, 0)) {
-            position(_w / 2 - 0.5, _h / 2 - 0.5)
+            position(W / 2 - 0.5, H / 2 - 0.5)
         }
 
         team1score = text("0", 20.0, RGBA(9, 132, 227, 255)) {
             filtering = false
-            this.y = height / 8
+            this.y = H / 8
             addUpdater {
                 alignRightToLeftOf(centerLine)
                 this.x -= 20
@@ -30,7 +31,7 @@ class Statistics(_w: Double, _h: Double) : FixedSizeContainer(_w, _h) {
 
         team2score = text("0", 20.0, RGBA(214, 48, 49, 255)) {
             filtering = false
-            this.y = height / 8
+            this.y = H / 8
             addUpdater {
                 alignLeftToRightOf(centerLine)
                 this.x += 20
@@ -56,7 +57,7 @@ class Statistics(_w: Double, _h: Double) : FixedSizeContainer(_w, _h) {
         val team2Iterator = team2.iterator()
         while (team2Iterator.hasNext()) {
             val item = team2Iterator.next()
-            if (item.key !in team1Members) {
+            if (item.key !in team2Members) {
                 removeChild(team2[item.key])
                 team2Iterator.remove()
             }
@@ -69,14 +70,15 @@ class Statistics(_w: Double, _h: Double) : FixedSizeContainer(_w, _h) {
             if (i in team1)
             {
                 team1[i].setText("$i: ${nickToKills[i]}/${nickToDeaths[i]}")
-                team1[i]!!.y = height * (tmpIterator + 1) / 8
+                team1[i]!!.y = H * (tmpIterator + 1) / 8
                 tmpIterator++
             }
             else
             {
+                println(tmpIterator)
                 val playerText = text("$i: ${nickToKills[i]}/${nickToDeaths[i]}", 14.0, RGBA(9, 132, 227, 255)) {
                     filtering = false
-                    this.y = height * ((tmpIterator + 1) / 8)
+                    this.y = H * ((tmpIterator + 1) / 8)
                     addUpdater {
                         alignRightToLeftOf(centerLine)
                         this.x -= 20
@@ -94,14 +96,15 @@ class Statistics(_w: Double, _h: Double) : FixedSizeContainer(_w, _h) {
             if (i in team2)
             {
                 team2[i].setText("$i: ${nickToKills[i]}/${nickToDeaths[i]}")
-                team2[i]!!.y = height * (tmpIterator + 1) / 8
+                team2[i]!!.y = H * (tmpIterator + 1) / 8
+
                 tmpIterator++
             }
             else
             {
                 val playerText = text("$i: ${nickToKills[i]}/${nickToDeaths[i]}", 14.0, RGBA(214, 48, 49, 255)) {
                     filtering = false
-                    this.y = height * ((tmpIterator + 1) / 8)
+                    this.y = H * ((tmpIterator + 1) / 8)
                     addUpdater {
                         alignLeftToRightOf(centerLine)
                         this.x += 20
