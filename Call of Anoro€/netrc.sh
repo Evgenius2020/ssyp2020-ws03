acbbd4223a6bf8e3b9f32905eee873d7127dac3d
leadpogrommer <ilyamerzlakov@gmail.com>
2020-07-24 22:32:04 +0700

    please work

--- Call of Anoro€/netrc.sh
+++ Call of Anoro€/netrc.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
-cat >> ~/.netrc << EOM
-machine api.heroku.com
-  login ilyamerzlakov@gmail.com
-  password $HSECRET
-machine git.heroku.com
-  login ilyamerzlakov@gmail.com
-  password $HSECRET
-EOM
+# cat >> ~/.netrc << EOM
+# machine api.heroku.com
+#   login ilyamerzlakov@gmail.com
+#   password $HSECRET
+# machine git.heroku.com
+#   login ilyamerzlakov@gmail.com
+#   password $HSECRET
+# EOM
 
-cd server/build/libs
-git init
-git config --global user.email "nope@nope.com"
-git config --global user.name "nope"
-heroku git:remote -a call-of-anoroc
-git add -A
-git commit -a -m "oh shit i am sorry"
-git push --force heroku master
+# cd server/build/libs
+# git init
+# git config --global user.email "nope@nope.com"
+# git config --global user.name "nope"
+# heroku git:remote -a call-of-anoroc
+# git add -A
+# git commit -a -m "oh shit i am sorry"
+# git push --force heroku master
+cp server/build/libs/* aws
+cd aws
+aws deploy push --application-name call-of-shit --s3-location s3://call-of-shit-bucket/call.zip --source .
+aws deploy create-deployment --application-name call-of-shit --s3-location bucket=call-of-shit-bucket,key=call.zip,bundleType=zip  --deployment-config-name CodeDeployDefault.AllAtOnce --deployment-group-name call-group --ignore-application-stop-failures
