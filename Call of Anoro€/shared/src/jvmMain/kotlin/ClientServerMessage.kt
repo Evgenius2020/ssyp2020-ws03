a2a6f4146567283d9970a5648beec11bbb300852
malevrovich <dddccc2017@mail.ru>
2020-07-24 13:16:36 +0700

    ClientServerMessage Register

--- Call of Anoro€/shared/src/jvmMain/kotlin/ClientServerMessage.kt
+++ Call of Anoro€/shared/src/jvmMain/kotlin/ClientServerMessage.kt
@@ -3,7 +3,7 @@ package shared
 import Statistic
 
 sealed class ClientServerMessage : java.io.Serializable
-//object Register : ClientServerMessage()
+class Register(val nick: String) : ClientServerMessage()
 object GetRenderInfo : ClientServerMessage()
 class SetAngle (val point : ClientServerPoint) : ClientServerMessage()
 object Shoot : ClientServerMessage()
