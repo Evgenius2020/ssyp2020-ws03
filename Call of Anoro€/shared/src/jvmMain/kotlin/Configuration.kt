9dba8023fd2e5a08e463ff6e540e3ab6621f6748
leadpogrommer <ilyamerzlakov@gmail.com>
2020-07-25 13:29:31 +0700

    balance

--- Call of Anoro€/shared/src/jvmMain/kotlin/Configuration.kt
+++ Call of Anoro€/shared/src/jvmMain/kotlin/Configuration.kt
@@ -6,13 +6,13 @@ object Configuration {
     const val radiusOfPlayer = 15.9
     const val radiusOfBullet = 2.0
     const val sizeOfObj = 16.0
-    const val speedOfPlayer = 1.0
+    const val speedOfPlayer = 2.0
     const val speedOfBullet = 5.0
-    const val shootCD = (1.5 * fps).toInt() //seconds * fps => frames
+    const val shootCD = (0.5 * fps).toInt() //seconds * fps => frames
     const val baseRespawnTime = 10 * fps //seconds * fps => frames
     const val gameTime = 90 * fps //seconds * fps => frames
     const val healthOfPlayer = 100
-    const val baseDamage = 34
+    const val baseDamage = 50
     const val width = 640.0
     const val height = 640.0
     const val boomDuration = 1 * fps // seconds * fps => frames
